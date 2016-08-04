d5f0fb8e11812e336bb2b4197cd47c0cddf3a26c
zlfkyo@sina.com <zlfkyo@sina.com>
2016-08-04 22:23:56 +0800

    j

---
+++ src/main/java/com/book1/t08/druid/firehose/StormCommitRunnable.java
@@ -0,0 +1,22 @@
+package com.book1.t08.druid.firehose;
+
+import com.esotericsoftware.minlog.Log;
+
+import java.util.List;
+
+public class StormCommitRunnable implements Runnable {
+    private List<Long> txIds = null;
+
+    public StormCommitRunnable(List<Long> txIds) {
+        this.txIds = txIds;
+    }
+
+    @Override
+    public void run() {
+        try {
+            StormFirehose.STATUS.complete(txIds);
+        } catch (Exception e) {
+            Log.error("Could not complete transactions.", e);
+        }
+    }
+}
